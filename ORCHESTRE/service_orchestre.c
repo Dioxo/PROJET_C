492b3c45aecee084643d66697088ef9069ab47f0
Dioxo <amolinares19@gmail.com>
2019-12-10 03:03:40 +0100

    base d'orchestre

--- ORCHESTRE/service_orchestre.c
+++ ORCHESTRE/service_orchestre.c
@@ -1,3 +1,10 @@
 #include "myassert.h"
 
 #include "service_orchestre.h"
+
+void orchestreWrite(AnonymeTube *anonyme, const void *buf, size_t size){
+  write(anonyme->fd[1], buf , size);
+}
+void serviceRead(AnonymeTube *anonyme, void *buf, size_t size){
+  read(anonyme->fd[0], buf , size);
+}
--- ORCHESTRE/service_orchestre.h
+++ ORCHESTRE/service_orchestre.h
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+
 #ifndef SERVICE_ORCHESTRE_H
 #define SERVICE_ORCHESTRE_H
 
@@ -5,5 +14,16 @@
 // - le tube anonyme pour que l'orchestre envoie des données au service
 // - le sémaphore pour que  le service indique à l'orchestre la fin
 //   d'un traitement
+
+
+typedef struct {
+    int fd[2];
+} AnonymeTube;
+
+/* =================================
+ 		Envois et réceptions
+   =================================  */
+void orchestreWrite(AnonymeTube *anonyme, const void *buf, size_t size);
+void serviceRead(AnonymeTube *anonyme, void *buf, size_t size);
 
 #endif
